e1514cf8f12c7fdd151e7fe77a96a805acc58218
Kawshal Chhetri <kawshalchhetri@gmail.com>
2016-01-10 15:17:47 +0530

    uu

---
+++ index.js
@@ -0,0 +1,29 @@
+var app=angular.module("ngAnimateApp",['ngAnimate']);
+
+app.controller("ngAnimateController", function($scope,$timeout) {
+    $scope.customers=[];
+    $scope.customer =  [
+        {name:"Rachel",description:"One More Day"},
+        {name:"Ross",description:"One More Day"},
+        {name:"Chandler",description:"One More Day"},
+        {name:"Phoebe",description:"One More Day"},
+        {name:"Monica",description:"One More Day"},
+        {name:"Joey",description:"One More Day"}
+    ];
+    $scope.load= function(){
+        angular.forEach($scope.customer,function(item){
+           $scope.customers.push(item);
+        })
+    };
+    $scope.addCustomers=function(){
+        var abc=  {name:"Rachel",description:"One More Day"};
+        $scope.customers.push(abc);
+    };
+    $scope.removeCustomers=function(index){
+        $scope.customers.splice(index,1);
+    }
+    $scope.init=function(){
+        $timeout(function(){$scope.load()}, 3000);
+    };
+    $scope.init();
+});
